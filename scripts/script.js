2bcd475bb066631b0baa3270fd52e94d5705f703
xtxme <TT.icy013@gmail.com>
2024-07-17 22:51:52 +0700

    แก้โค้ด เสร็จจิงๆละ

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ const firstNameInput = document.querySelector("#first-name-input");
 const lastNameInput = document.querySelector("#last-name-input");
 const emailInput = document.querySelector("#email-input");
 const passwordInput = document.querySelector("#password-input");
-const confirmPasswordInput = document.querySelect("#password-confirm-input");
+const confirmPasswordInput = document.querySelector("#password-confirm-input");
 
 // create reference for buttons.
 const submitBtn = document.querySelector("#submit-btn");
@@ -46,11 +46,11 @@ confirmPasswordInput.onkeyup = () => {
 
 // add callback function for submit button.
 submitBtn.onclick = () => {
-  isFirstNameOk = false;
-  isLastNameOk = false;
-  isEmailOk = false;
-  isPasswordOk = false;
-  isPasswordConfirmOk = false;
+  let isFirstNameOk = false;
+  let isLastNameOk = false;
+  let isEmailOk = false;
+  let isPasswordOk = false;
+  let isPasswordConfirmOk = false;
 
   // validate first name
   if (firstNameInput.value == "") {
@@ -85,11 +85,12 @@ submitBtn.onclick = () => {
   }
 
   // validate confirm password
-  if (confirmPasswordInput.value !== passwordInput.value && confirmPasswordInput.value.length <=6) {
+  if (confirmPasswordInput.value.length < 6 || confirmPasswordInput.value !== passwordInput.value) {
     confirmPasswordInput.classList.add("is-invalid");
   } else {
+    confirmPasswordInput.classList.remove("is-invalid");
     confirmPasswordInput.classList.add("is-valid");
-    isPasswordConfirmOk = true;
+    isConfirmPasswordOk = true;
   }
 
   if (isFirstNameOk && isLastNameOk && isEmailOk && isPasswordOk && isPasswordConfirmOk) 
@@ -97,24 +98,18 @@ submitBtn.onclick = () => {
 };
 
 // add callback function for Reset button.
-resetBtn.onclick = () => {
+resetBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+
   firstNameInput.value = "";
-  firstNameInput.classList.remove("is-invalid");
-  firstNameInput.classList.remove("is-valid");
-
   lastNameInput.value = "";
-  lastNameInput.classList.remove("is-invalid");
-  lastNameInput.classList.remove("is-valid");
-
   emailInput.value = "";
-  emailInput.classList.remove("is-invalid");
-  emailInput.classList.remove("is-valid");
-
   passwordInput.value = "";
-  passwordInput.classList.remove("is-invalid");
-  passwordInput.classList.remove("is-valid");
-
   confirmPasswordInput.value = "";
-  confirmPasswordInput.classList.remove("is-invalid");
-  confirmPasswordInput.classList.remove("is-valid");
-};
+
+  firstNameInput.classList.remove("is-invalid", "is-valid");
+  lastNameInput.classList.remove("is-invalid", "is-valid");
+  emailInput.classList.remove("is-invalid", "is-valid");
+  passwordInput.classList.remove("is-invalid", "is-valid");
+  confirmPasswordInput.classList.remove("is-invalid", "is-valid");
+});
